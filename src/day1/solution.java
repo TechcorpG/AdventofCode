4b678e43d9001f682a854ed8d18f4655246ffee3
TechcorpG <niels.moeller2002@gmail.com>
2023-12-01 12:59:28 +0100

    Day 1 Initial Build

--- src/day1/solution.java
+++ src/day1/solution.java
@@ -1,12 +1,34 @@
 package day1;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class solution {
     public static void main(String[] args) {
 
-        System.out.println("Moin!");
+        Scanner in = new Scanner(new FileReader("input.txt"));
+
+        while(in.hasNext()){
+            String temp = in.nextLine();
+            int max = temp.length();
+            Pattern pattern = Pattern.compile("\\d");
+
+            for(int i=0;i<max;i++){
+                char c = temp.charAt(i);
+
+                Matcher matcher = pattern.matcher(String.valueOf(c));
+
+            }
+
+            while(matcher.find()){
+                
+            }
+
+
+        }
 
     }
 }
