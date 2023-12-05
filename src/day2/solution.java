856f4d90fd8ec4af3d2d35cc5b072dfb6f424704
TechcorpG <niels.moeller2002@gmail.com>
2023-12-05 21:27:11 +0100

    Initial Day 2

--- src/day2/solution.java
+++ src/day2/solution.java
@@ -1,4 +1,4 @@
-package template;
+package day2;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class solution {
     public static void main(String[] args) throws FileNotFoundException {
 
-        System.out.println("Moin!");
-
         Scanner in = new Scanner(new File("src/template/input.txt"));
+
+        System.out.println("Moin!");
 
     }
 }
